513c4322d5e850cf98d5a852a1976547b09c09bb
xingyu yao <xy2779@nyu.edu>
2024-10-19 15:53:04 -0400

    notes

--- 1/sketch.js
+++ 1/sketch.js
@@ -2,19 +2,32 @@
 
 let ypos;
 
+let aCircle = {};
+let aList = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
 
-  ypos = -50;
+  aList = [0,3,4,6];
+  aCircle = {
+    x:width/2,
+    y:height/2,
+    d:60,
+    a:255
+  };
+
 }
 
 function draw() {
   background(220, 20, 220);
-  ellipse(width / 2, ypos, 50);
-  ypos += 10;
+  //ellipse(width / 2, ypos, 50);
+  fill(255,aCircle["a"]);
 
-  if (ypos > height) {
-    ypos = -50;
-  }
+  ellipse(aCircle.x,aCircle.y,aCircle.d)
+
+  // aCircle.a += 4;
+  aCircle.a =(aCircle.a + 4) % 255;
+//aCircle.x = aCircle["x"]
+
 }
--- 3/sketch.js
+++ 3/sketch.js
@@ -1,8 +1,8 @@
 // TODO: visualize data from JSON
 
-let data= [];
+let data = [];
 
-function preload(){
+function preload() {
   data = loadJSON("wine.json");
 }
 
@@ -12,15 +12,15 @@ function setup() {
   noFill();
   stroke("red");
 
- // data = loadJSON("wine.json");
-data = Object.values(data);
+  // data = loadJSON("wine.json");
+  data = Object.values(data);
   print(data[0]);
-  for (let idx = 0;idx<data.length;idx++){
+  for (let idx = 0; idx < data.length; idx++) {
     let aWine = data[idx];
-    let x = map(idx,0,data.length,0,width)
-    let d = map(aWine.price,0,1000,0,height)
+    let x = map(idx, 0, data.length, 0, width);
+    let d = map(aWine.price, 0, 1000, 0, height);
 
-    ellipse(x, height/2, d)
+    ellipse(x, height / 2, d);
   }
 }
 
